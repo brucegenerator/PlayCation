9b7513700d6362e7fd33d1be456599510ab6fc9c
bruce generator <rothje@vcu.edu>
2021-05-24 18:31:17 -0400

    installed react-router-dom and established some preliminary routes for Users and Places

--- play-cation/src/App.js
+++ play-cation/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Route path="/" exact>
+        <Users></Users>
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace />
+      </Route>
+      <Redirect to="/" />
+    </Router>
   );
 }
 
