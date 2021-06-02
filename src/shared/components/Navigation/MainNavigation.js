0ec41547b44b9a60688f3be00d36eb358ac22f12
bruce generator <rothje@vcu.edu>
2021-06-01 21:37:46 -0400

    started work on the sidedrawer, currently overlays everything else and is not controlled by state

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,12 +1,21 @@
 import React from "react";
 import MainHeader from "./MainHeader";
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+
 import { Link } from 'react-router-dom'
 import "./MainNavigation.css";
 
-import NavLinks from './NavLinks';
+
 
 const MainNavigation = (props) => {
   return (
+    <React.Fragment>
+    <SideDrawer>
+      <nav className="main-navigation__drawer-nav">
+        <NavLinks />
+      </nav>
+    </SideDrawer>
     <MainHeader>
       <button className="main-navigation__menu-btn">
       <span></span>
@@ -16,10 +25,11 @@ const MainNavigation = (props) => {
       <h1 className="main-navigation__title">
         <Link to="/">Your Places</Link>
       </h1>
-      <nav>
+      <nav className="main-navigation__header-nav">
         <NavLinks />
       </nav>
     </MainHeader>
+    </React.Fragment>
   );
 };
 
