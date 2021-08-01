b447d8ca8b5aeee141749c341513c3236fb4f904
bruce generator <rothj1987@gmail.com>
2021-08-01 19:11:07 -0400

    added error handling model for routes

--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -1,10 +1,47 @@
 const express = require("express");
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "It just works" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "Famous tall building",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    return next(new HttpError('Could not find a place for the provided id', 404));
+  }
+
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((u) => {
+    return u.creator === userId;
+  });
+
+  if (!place) {
+    return next(new HttpError('Could not find a place for the provided user id', 404));
+  }
+
+  res.json({ place });
 });
 
 module.exports = router;
